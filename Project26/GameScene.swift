a45e067e38cabd0a022807edbd019c20d7a1e211
jslusser <jslusser@gmail.com>
2017-07-20 09:44:09 -0400

    BitMask, contactTestBitMask

--- Project26/GameScene.swift
+++ Project26/GameScene.swift
@@ -9,11 +9,26 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
 
 class GameScene: SKScene {
     
     
     override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        loadLevel()
         
           }
     
@@ -29,12 +44,50 @@ class GameScene: SKScene {
                         
                         if letter == "x" {
                             // load wall
+                            let node = SKSpriteNode(imageNamed: "block")
+                            node.position = position
+                            
+                            node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                            node.physicsBody!.categoryBitMask = CollisionTypes.wall.rawValue
+                            node.physicsBody!.isDynamic = false
+                            addChild(node)
                         } else if letter == "v"  {
                             // load vortex
+                            let node = SKSpriteNode(imageNamed: "vortex")
+                            node.name = "vortex"
+                            node.position = position
+                            node.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat.pi, duration: 1)))
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.isDynamic = false
+                            
+                            node.physicsBody!.categoryBitMask = CollisionTypes.vortex.rawValue
+                            node.physicsBody!.contactTestBitMask = CollisionTypes.player.rawValue
+                            node.physicsBody!.collisionBitMask = 0
+                            addChild(node)
                         } else if letter == "s"  {
                             // load star
+                            let node = SKSpriteNode(imageNamed: "star")
+                            node.name = "star"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.isDynamic = false
+                            
+                            node.physicsBody!.categoryBitMask = CollisionTypes.star.rawValue
+                            node.physicsBody!.contactTestBitMask = CollisionTypes.player.rawValue
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
                         } else if letter == "f"  {
                             // load finish
+                            let node = SKSpriteNode(imageNamed: "finish")
+                            node.name = "finish"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.isDynamic = false
+                            
+                            node.physicsBody!.categoryBitMask = CollisionTypes.finish.rawValue
+                            node.physicsBody!.contactTestBitMask = CollisionTypes.player.rawValue
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
                         }
                     }
                 }
